96b52ec75f415283d7875256a72a419398002132
Daniella Dutra <danivetdutra@gmail.com>
2022-03-04 13:51:21 -0300

    Exemplo toUpperCase e toLocaleLowerCase

---
+++ métodos/script.js
@@ -0,0 +1,8 @@
+//Métodos são propriedades que funcionam como funções, strings e arrays contém métodos, servem para alterar os dados.
+
+let marca = "Nike";
+console.log(typeof marca.toUpperCase); // função
+console.log(marca.toUpperCase()); //transforma em maiusculo
+
+let marca2 = marca.toUpperCase();
+console.log(marca.toLocaleLowerCase()); //transforma em minusculo
