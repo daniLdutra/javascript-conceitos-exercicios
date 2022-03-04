bcea570c5c28cfb161bb7ea84f6b7c17a0212bd7
Daniella Dutra <danivetdutra@gmail.com>
2022-03-04 11:39:27 -0300

    Manipular arrays

---
+++ arrays/script.js
@@ -0,0 +1,27 @@
+//Posibilita adicionar um conjunto de valores a uma variável;
+//Ex: lista de dados de busca: lista de nomes, lista de produtos, lista de valores.
+
+let arr = [1, 4, 5, 6, 7];
+let nomes = ['dani', 'ilan', 'elza'];
+let bool = [true, false, true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+
+//Para acessar um indice do array:
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(arr[arr.length - 1]);
+
+// Propriedades: são informações que podem ser verificadas de um valor;
+
+let nome = 'João';
+console.log(nome.length);
+
+let numeros = [1, 23, 34, 5, 6, 7, 8];
+console.log(numeros.length);
+
+console.log(numeros[1]); //elemento que entrega o indices
