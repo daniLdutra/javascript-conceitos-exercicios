8ab3b7e758ec34227010b29a6b63bd4cb0341edf
Daniella Dutra <danivetdutra@gmail.com>
2022-03-04 13:25:52 -0300

    Metodos de array: indexOf e  lastIndexOf

---
+++ métodos de array: indexOf e lastIndexOf/script.js
@@ -0,0 +1,6 @@
+// indexOf: encontra o índice de um determinado elemento;
+// lastIndexOf: enccontra o último índice de um elemento;
+
+let numero = [1, 0, 2, 1, 0];
+console.log(numero.indexOf(0)); //indice 1
+console.log(numero.lastIndexOf(0)); // indice 4
