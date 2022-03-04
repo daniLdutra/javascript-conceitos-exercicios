d9071548b9686fa61604edb430a996a94ea3fb93
Daniella Dutra <danivetdutra@gmail.com>
2022-03-04 13:15:00 -0300

    Metodo padStart

---
+++ método de string: padStart/script.js
@@ -0,0 +1,7 @@
+// Insere caracteres antes da String.
+let sku = '34';
+console.log(sku.padStart(6, '0'));
+//sku código de produto muito usado nas e-commerce ou lojas.
+
+let sku2 = '7225';
+console.log(sku2.padStart(6, '0'));
